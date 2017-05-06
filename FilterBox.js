ac9aa55aa8c3f91f423671035afaf988760c5239
Jacob <jgsteves@uwaterloo.ca>
2017-05-06 16:39:26 -0400

    Added preset link functionality
    The preset can now be clicked and it works the same as if the information was typed.

--- FilterBox.js
+++ FilterBox.js
@@ -32,7 +32,9 @@ class FilterBox extends React.Component {
     this.state = {
       inputValue: '',
       open: false,
-      Response: 'none',
+      Response: '',
+      currentIndex: -1,
+      lastVal: '',
       courseListing: ['CS100', 'CS135', 'CS136', 'CS240', 'CS241'],
       courseBackup: ['CS100', 'CS135', 'CS136', 'CS240', 'CS241']
     };
@@ -41,30 +43,6 @@ class FilterBox extends React.Component {
   static propTypes: {
     max: React.PropTypes.number.isRequired,
     handleSelect: React.PropTypes.func.isRequired
-  }
-
-  printNames(){
-    const { courseListing } = this.state;
-    let items = courseListing.slice(0, this.props.max + 1);
-    let remaining = courseListing.length - items.length;
-    let count = 0;
-    let response = 'And ' + remaining + ' others';
-    return items.map((cur, i) => {
-      if(count != this.props.max){
-        ++count;
-        return (
-          <div className='FilterBoxItem' key={i}>
-            {cur}
-          </div>
-        );
-      } else if(remaining > 0) {
-        return (
-          <div className='FilterBoxItem' key={i}>
-            <i>{response}</i>
-          </div>
-        );
-      }
-    });
   }
 
   handleInputChange(e) {
@@ -81,21 +59,41 @@ class FilterBox extends React.Component {
     });
   }
 
-  handleOffFocus(){
-    this.setState({ open: !this.state.open })
+  handleOffFocus(e){
+    const { currentIndex } = this.state;
+    const { courseBackup } = this.state
+    if(currentIndex >= 0){
+      const value = courseBackup[currentIndex];
+      this.setState({ currentIndex: -1 });
+      this.handlePresetClick(e, value);
+    } else {
+      this.setState({ open: false })
+    }
   }
 
   handleInputSubmit(e){
     e.preventDefault();
-    const { inputValue } = this.state;
-    let courseListing = this.state.courseBackup.map((cur, i) => {
-      return cur.toLowerCase();
-    });
-    const index = courseListing.indexOf(inputValue.toLowerCase());
-    this.setState({
-      courseListing: []
-    });
-    this.handleSelect(inputValue, index);
+    blur();
+    const { currentIndex } = this.state;
+    if(currentIndex >= 0) {
+      const { courseBackup } = this.state;
+      const value = courseBackup[currentIndex];
+      this.setState({
+        lastVal: value
+      });
+      this.handlePresetClick(e, value);
+    } else {
+      const { inputValue } = this.state;
+      let courseListing = this.state.courseBackup.map((cur, i) => {
+        return cur.toLowerCase();
+      });
+      const index = courseListing.indexOf(inputValue.toLowerCase());
+      this.setState({
+        courseListing: [],
+        lastVal: inputValue
+      });
+      this.handleSelect(inputValue, index);
+    }
   }
 
   handleSelect(choice, index){
@@ -106,6 +104,77 @@ class FilterBox extends React.Component {
     }
   }
 
+  handlePresetClick(e, value) {
+    e.preventDefault();
+    let courseListing = this.state.courseBackup.map((cur, i) => {
+      return cur.toLowerCase();
+    });
+    const index = courseListing.indexOf(value.toLowerCase());
+    this.setState({
+      open: false,
+      currentIndex: -1
+    });
+    this.handleSelect(value, index);
+  }
+
+  handlePresetEnter(value){
+    let courseListing = this.state.courseBackup.map((cur, i) => {
+      return cur.toLowerCase();
+    });
+    const index = courseListing.indexOf(value.toLowerCase());
+    this.setState({
+      currentIndex: index
+    });
+  }
+
+  handlePresetExit(){
+    this.setState({currentIndex: -1})
+  }
+
+  handleKeyDown(e){
+    switch(e.keyCode){
+      case 38: //This is the up arrow
+      console.log("up");
+      break;
+
+      case 40: //This is the down arrow
+      console.log("down");
+      break
+
+      case 13: //This is enter
+      console.log("enter");
+      break;
+    }
+  }
+
+  printNames(){
+    const { courseListing } = this.state;
+    let items = courseListing.slice(0, this.props.max + 1);
+    let remaining = courseListing.length - items.length;
+    let count = 0;
+    let response = 'And ' + remaining + ' others';
+    return items.map((cur, i) => {
+      if(count != this.props.max){
+        ++count;
+        return (
+          <div className='FilterBoxItem' key={i}
+            onClick={(e) => this.handlePresetClick(e, cur)}
+            onMouseEnter={() => this.handlePresetEnter(cur)}
+            onMouseLeave={() => this.handlePresetExit()}
+            onKeyDown={(e) => this.handleKeyDown(e)}>
+            {cur}
+          </div>
+        );
+      } else if(remaining > 0) {
+        return (
+          <div className='FilterBoxItem' key={i}>
+            <i>{response}</i>
+          </div>
+        );
+      }
+    });
+  }
+
   renderContent() {
     return (
       <div className="FilterBoxDropDown">
@@ -113,7 +182,7 @@ class FilterBox extends React.Component {
           <input
             onFocus={(e) => this.handleInputClick(e)}
             onChange={(e) => this.handleInputChange(e)}
-            onBlur={() => this.handleOffFocus()}
+            onBlur={(e) => this.handleOffFocus(e)}
             type="text"
             className="FilterBoxInput">
           </input>
