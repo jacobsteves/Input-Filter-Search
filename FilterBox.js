0f7c0018727532854796035eaf42ff6008141ff2
Jacob <jgsteves@uwaterloo.ca>
2017-05-07 00:35:04 -0400

    Implemented FilterBox receiving list as prop

--- FilterBox.js
+++ FilterBox.js
@@ -3,47 +3,26 @@ import '../stylesheets/FilterBox.css';
 
 'use strict'
 
-
-var defaultMaxText = '+# more not shown';
-
-var defaultFilter = function(filterText, optionName) { // also optionIndex as third arg
-  return (optionName.toLowerCase().indexOf(filterText.toLowerCase()) >= 0);
-};
-
-var genLength = function(list) {
-  // deal with both regular arrays and immutablejs objects, which have .count() instead of length
-  return (typeof list.count !== 'undefined' ? list.count() : list.length);
-};
-
-var genGet = function(list, i) {
-  // deal with both regular arrays and immutablejs objects, which have list.get(i) instead of list[i]
-  return (typeof list.get !== 'undefined' ? list.get(i) : list[i]);
-};
-
-var caseInsensIndexOf = function(list, str) {
-  var lowerList = list.map(function(item) { return item.toLowerCase(); });
-  return lowerList.indexOf(str.toLowerCase());
-};
-
 class FilterBox extends React.Component {
 
   constructor() {
     super();
     this.state = {
       inputValue: '',
-      open: false,
-      Response: '',
-      currentIndex: -1,
-      lastVal: '',
       courseListing: ['CS100', 'CS135', 'CS136', 'CS240', 'CS241'],
-      courseBackup: ['CS100', 'CS135', 'CS136', 'CS240', 'CS241'],
-      clickedList: ['n','n','n','n','n'] // 'n' = no, not clicked; 'y' = yes, clicked
+      courseBackup: ['CS100', 'CS135', 'CS136', 'CS240', 'CS241']
     };
   }
 
-  static propTypes: {
-    max: React.PropTypes.number.isRequired,
-    handleSelect: React.PropTypes.func.isRequired
+  printNames(){
+      const { courseListing } = this.state;
+      return courseListing.map((cur, i) => {
+        return (
+          <div className='item' key={i}>
+            {cur}
+          </div>
+        );
+      });
   }
 
   handleInputChange(e) {
@@ -51,205 +30,25 @@ class FilterBox extends React.Component {
       inputValue: e.target.value,
       courseListing: this.state.courseBackup.filter((cur) => {return cur.toLowerCase().includes(e.target.value.toLowerCase())})
     })
-  }
-
-  handleInputClick(e) {
-    e.preventDefault(),
-    this.setState({
-      open: !this.state.open
-    });
-  }
-
-  handleOffFocus(e){
-    const { currentIndex } = this.state;
-    const { courseBackup } = this.state
-    if(currentIndex >= 0){
-      const value = courseBackup[currentIndex];
-      this.setState({ currentIndex: -1 });
-      this.handlePresetClick(e, value);
-    } else {
-      this.setState({ open: false })
-    }
-  }
-
-  handleInputSubmit(e){
-    e.preventDefault();
-    blur();
-    const { currentIndex } = this.state;
-    if(currentIndex >= 0) {
-      const { courseBackup } = this.state;
-      const value = courseBackup[currentIndex];
-      this.setState({
-        lastVal: value
-      });
-      this.handlePresetClick(e, value);
-    } else {
-      const { inputValue } = this.state;
-      let courseListing = this.state.courseBackup.map((cur, i) => {
-        return cur.toLowerCase();
-      });
-      const index = courseListing.indexOf(inputValue.toLowerCase());
-      this.setState({
-        courseListing: [],
-        lastVal: inputValue
-      });
-      this.handleSelect(inputValue, index);
-    }
-  }
-
-  handleSelect(choice, index){
-    if (index>=0) {
-      this.setState({Response: choice + ' is a nice choice'});
-    } else {
-      this.setState({Response: choice + ' isn\'t on the list!'});
-    }
-  }
-
-  handlePresetClick(e, value) {
-    e.preventDefault();
-    let courseListing = this.state.courseBackup.map((cur, i) => {
-      return cur.toLowerCase();
-    });
-    const index = courseListing.indexOf(value.toLowerCase());
-    this.setState({
-      open: false,
-      currentIndex: -1
-    });
-    this.handleSelect(value, index);
-  }
-
-  handlePresetEnter(value){
-    let courseListing = this.state.courseBackup.map((cur, i) => {
-      return cur.toLowerCase();
-    });
-    const index = courseListing.indexOf(value.toLowerCase());
-    let newClicked = this.state.clickedList;
-    newClicked[index] = 'y';
-    this.setState({
-      currentIndex: index,
-      clickedList: newClicked
-    });
-  }
-
-  handlePresetExit(value){
-    let courseListing = this.state.courseBackup.map((cur, i) => {
-      return cur.toLowerCase();
-    });
-    const index = courseListing.indexOf(value.toLowerCase());
-    let newClicked = this.state.clickedList;
-    newClicked[index] = 'n';
-    this.setState({
-      currentIndex: -1,
-      clickedList: newClicked
-    })
-  }
-
-  handleKeyDown(e){
-    let index = this.state.currentIndex;
-    switch(e.keyCode){
-      case 38: //This is the up arrow
-      console.log("up");
-      if(index >= 1) {
-        let newClicked = this.state.clickedList;
-        newClicked[index] = 'n';
-        newClicked[index - 1] = 'y';
-        this.setState({
-          currentIndex: index - 1,
-          clickedList: newClicked
-        })
-      } else {
-        let newClicked = this.state.clickedList;
-        newClicked[0] = 'n';
-        newClicked[this.props.max] = 'y';
-        this.setState({
-          currentIndex: this.props.max,
-          clickedList: newClicked
-        })
-      }
-      console.log(index);
-      break;
-
-      case 40: //This is the down arrow
-      console.log("down");
-      if(index < this.props.max) {
-        let newClicked = this.state.clickedList;
-        newClicked[index] = 'n';
-        newClicked[index + 1] = 'y';
-        this.setState({
-          currentIndex: index + 1,
-          clickedList: newClicked
-        })
-      } else {
-        let newClicked = this.state.clickedList;
-        newClicked[0] = 'y';
-        newClicked[this.props.max] = 'n';
-        this.setState({
-          currentIndex: 0,
-          clickedList: newClicked
-        })
-      }
-      console.log(index);
-      break;
-
-      case 13: //This is enter
-      console.log("enter");
-      break;
-    }
-  }
-
-  printNames(){
-    const { courseListing } = this.state;
-    let items = courseListing.slice(0, this.props.max + 1);
-    let remaining = courseListing.length - items.length;
-    let count = 0;
-    let response = 'And ' + remaining + ' others';
-    return items.map((cur, i) => {
-      if(count != this.props.max){
-        ++count;
-        return (
-          <div className={this.state.clickedList[count - 1]} key={i}
-            onClick={(e) => this.handlePresetClick(e, cur)}
-            onMouseEnter={() => this.handlePresetEnter(cur)}
-            onMouseLeave={() => this.handlePresetExit(cur)}>
-            {cur}
-          </div>
-        );
-      } else if(remaining > 0) {
-        return (
-          <div className='n' key={i}>
-            <i>{response}</i>
-          </div>
-        );
-      }
-    });
+    console.log(this.state.courseListing);
   }
 
   renderContent() {
     return (
-      <div className="FilterBoxDropDown">
-        <form onSubmit={(e) => this.handleInputSubmit(e)}>
-          <input
-            onFocus={(e) => this.handleInputClick(e)}
-            onChange={(e) => this.handleInputChange(e)}
-            onBlur={(e) => this.handleOffFocus(e)}
-            onKeyDown={(e) => this.handleKeyDown(e)}
-            type="text"
-            className="FilterBoxInput">
-          </input>
-            <div className="divider">
-            { this.state.open ? this.printNames() : null }
-            </div>
-            <p>{ this.state.Response }</p>
-        </form>
-      </div>
+      <form onSubmit={(e) => preventDefault(e)}>
+      <input onChange={(e) => this.handleInputChange(e)} type="text"></input>
+      </form>
     );
   }
 
   render() {
     return (
+      <center>
       <div className='results'>
         { this.renderContent() }
+        { this.printNames() }
       </div>
+      </center>
     );
   }
 }
